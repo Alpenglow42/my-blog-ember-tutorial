8aecbf9fbe02171a408ddb4de498f94a70746085
Topher42 <gnrforlife@gmail.com>
2020-02-20 17:29:20 +0700

    first

---
+++ app/models/post.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+const {Model, attr} = DS;
+
+export default class PostModel extends Model {
+    @attr() title;
+    @attr() body;
+    @attr('date') publishedAt; 
+
+}
