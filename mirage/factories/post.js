7dc2329b54c19493271957cb0e994ef9812ed150
Topher42 <gnrforlife@gmail.com>
2020-02-24 12:05:06 +0700

    second

--- mirage/factories/post.js
+++ mirage/factories/post.js
@@ -1,5 +1,5 @@
 import { Factory } from 'ember-cli-mirage';
-import faker from 'faker';
+import faker from 'faker'; /* faker js imports fake data to use https://github.com/marak/Faker.js/ */
 
 export default Factory.extend({
     title() {
--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,5 +1,6 @@
 export default function(server) {
-  server.createList('posts',5);
+  server.createList('post',5); /* 5 here corresonds to number of lines
+  posted if I changed it to 10 I would see 10 lines*/
 
   /*
     Seed your development database using your factories.
